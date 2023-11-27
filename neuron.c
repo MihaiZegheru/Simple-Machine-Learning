491edc960cb49fb8cf09c86dff73609cab837720
MihaiZ <mihai.z.2005@gmail.com>
2023-11-27 17:33:50 +0200

    Added neuron init
    Signed-off-by: MihaiZ <mihai.z.2005@gmail.com>

--- neuron.c
+++ neuron.c
@@ -2,6 +2,8 @@
 
 #include <stdlib.h>
 
+#include <random_utils.h>
+
 neuron_t *neuron_new(size_t weights_size)
 {
     neuron_t *neuron = malloc(sizeof(neuron_t));
@@ -16,8 +18,10 @@ void neuron_delete(neuron_t *neuron)
     free(neuron);
 }
 
-
 void neuron_init(neuron_t *neuron)
 {
-
+    for (size_t i = 0; i < neuron->weights_size; i++) {
+        neuron->weights[i] = random_float_normalised();
+    }
+    neuron->bias = random_float_normalised();
 }
